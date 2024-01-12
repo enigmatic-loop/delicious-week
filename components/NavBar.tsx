93f839728fde958691daa403766f92e27f04b94a
nina <nina.sohn@gmail.com>
2024-01-11 23:56:21 -0800

    Start NavBar component

---
+++ components/NavBar.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import Link from "next/link";
+
+export default function NavBar() {
+  return (
+  <nav className="flex justify-center space-x-8 font-sans text-2xl bg-cyan-600 shadow-md text-white p-4">
+    <Link href="/home">Home</Link>
+    <Link href="/browse">Browse</Link>
+    <Link href="/grocery">Grocery</Link>
+  </nav>
+  );
+}
